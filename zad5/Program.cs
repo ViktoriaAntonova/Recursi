1bbb62bf41c4383cee6b9ac59bf571f40185aa74
yodatime <electriclikethor@gmail.com>
2025-02-13 18:45:13 +0200

    zad5

---
+++ zad5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Write number: ");
+            int num = int.Parse(Console.ReadLine());  
+            if (isEven(num))
+            {
+                Console.WriteLine($"{num} - even");
+            }
+            else
+            {
+                Console.WriteLine($"{num} - odd");
+            }
+        }
+
+        static bool isEven(int n)
+        {
+            if (n == 0)
+            {
+                return true;
+            }
+            if (n == 1)
+            {
+                return false;
+            }
+            return isEven(n - 2);
+        }
+    }
+}
