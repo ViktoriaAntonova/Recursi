ff739d45616b8d44e26aa42325fd5ba00b796c7c
yodatime <electriclikethor@gmail.com>
2025-02-13 18:14:49 +0200

    Add project files.

---
+++ recursi zad3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursi_zad3
+{
+    internal class Program
+    {
+        static int Reverse(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (number % 10) * (int)Math.Pow(10, (int)Math.Log10(number)) + Reverse(number / 10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int reversed = Reverse(number);
+            Console.WriteLine(reversed);
+        }
+    }
+}
