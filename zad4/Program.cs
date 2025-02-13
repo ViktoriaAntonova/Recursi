4a7d27bb752ead3154d636deda80398ac4126856
yodatime <electriclikethor@gmail.com>
2025-02-13 18:28:05 +0200

    zad4

---
+++ zad4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class Program
+    {
+        static int Power(int a, int b)
+        {
+            if (b == 0)
+            {
+                return 1;
+            }
+            return a * Power(a, b - 1);
+        }
+
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int result = Power(a, b);
+            Console.WriteLine($"{a} ^ {b} = {result}");
+        }
+    }
+}
